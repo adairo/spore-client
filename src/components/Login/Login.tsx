23a8f6bdbc6149e501b4118a317b82035645de92
adairo <adair.reyes450@gmail.com>
2023-07-05 18:27:36 -0600

    use Card component on LoginForm component

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,98 @@
+import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const loginSchema = z.object({
+  email: z.string().email("Ingresa un correo válido"),
+  password: z
+    .string()
+    .min(8, { message: "La contraseña debe ser de al menos 4 caracteres" }),
+});
+
+function LoginForm() {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  console.log(form);
+
+  const onSubmit = (data: z.infer<typeof loginSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="grid w-full place-items-center bg-slate-50 px-4">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>Inicia sesión</CardTitle>
+          <CardDescription>Empieza a administrar tus autos desde SporeCar</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form id="login-form" onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Correo</FormLabel>
+                    <FormControl>
+                      <Input placeholder="ejemplo@hotmail.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Contraseña</FormLabel>
+                    <FormControl>
+                      <Input type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="flex flex-col gap-2">
+          <Button form="login-form" type="submit" className="w-full text-base">
+            Iniciar sesión
+          </Button>
+          <Button variant={"outline"} className="w-full text-base ">
+            Crear cuenta
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export default LoginForm;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import LoginForm from './App.tsx'
-import "./main.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LoginForm from "@/components/Login/Login";
+import "./main.css";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const Root = () => {
+  return (
+    <div className="min-h-screen grid grid-rows-[auto_1fr]">
+      <header className="px-4 py-3 text-white bg-black text-xl font-bold">
+        SporeCar
+      </header>
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "login",
+        element: <LoginForm />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <LoginForm />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
